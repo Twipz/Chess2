15b9f0fe4cde5e0ef9bab095cf7f4d193c355b5e
Афанасьев Дмитрий Вадимович <ip711s01@mwp06.csc.local>
2018-02-27 12:43:24 +0700

    Creating main file

---
+++ chess.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("8 r n b q k b n r\n");
+    printf("7 p p p p p p p p\n");
+    printf("6\n5\n4\n3\n");
+    printf("2 P P P P P P P P\n");
+    printf("1 R N B Q K B N R\n");
+}
